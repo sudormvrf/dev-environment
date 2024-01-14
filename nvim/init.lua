c8679958e40ffdfe37e58b2e8f76b51854cb8769
John Hudgins <john.d.hudgins@gmail.com>
2024-01-14 07:01:19 -0500

    Initial development environment setup with neovim

---
+++ nvim/init.lua
@@ -0,0 +1,4 @@
+require("jhudgins.core.keymaps")
+require("jhudgins.core.options")
+require("jhudgins.lazy")
+
---
+++ nvim/lua/jhudgins/core/keymaps.lua
@@ -0,0 +1,24 @@
+vim.g.mapleader = " "
+
+local keymap = vim.keymap
+
+keymap.set("i", "jk", "<ESC>")
+
+keymap.set("n", "<leader>nh", ":nohl<CR>")
+
+keymap.set("n", "x", '"_x')
+
+keymap.set("n", "<leader>+", "<C-a>")
+keymap.set("n", "<leader>-", "<C-x>")
+
+keymap.set("n", "<leader>sv", "<C-w>v") -- split window vertically
+keymap.set("n", "<leader>sh", "<C-w>s") -- split window horizontally
+keymap.set("n", "<leader>se", "<C-w>=") -- make split windows equal width
+keymap.set("n", "<leader>sx", ":close<CR>") -- close current split window
+
+keymap.set("n", "<leader>to", ":tabnew<CR>") -- open new tab
+keymap.set("n", "<leader>tx", ":tabclose<CR>") -- close current tab
+keymap.set("n", "<leader>tn", ":tabn<CR>") -- go to next tab
+keymap.set("n", "<leader>tp", ":tabp<CR>") -- go to the previous tab
+
+
